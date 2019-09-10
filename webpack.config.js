dc49f0c920b02bc8264eae685dc174eecf61b0fa
liujian02 <liujian02@sunlands.com>
2019-09-10 17:30:14 +0800

    update package

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = (webpackConfig, env) => {
-    console.log(JSON.stringify(webpackConfig))
     webpackConfig.module.rules.forEach(rule => {
         const { loader = '', test } = rule
         if (loader.indexOf('url-loader') !== -1) {
